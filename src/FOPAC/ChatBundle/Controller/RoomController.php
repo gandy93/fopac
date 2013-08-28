2921c69448e30eaccb9ba74ee2f4c1a14b6d90c2
Gandalf <gandy93@gmail.com>
2013-08-28 17:32:07 +0200

    Finish sidebar

--- src/FOPAC/ChatBundle/Controller/RoomController.php
+++ src/FOPAC/ChatBundle/Controller/RoomController.php
@@ -48,6 +48,34 @@ class RoomController extends Controller
     	}
     }
 
+    public function editAction(Request $r)
+    {
+    	if ($r->isXmlHttpRequest() && $r->getMethod() == "POST") {
+    		$roomData = $r->get('room') ;  $auth = $r->get('auth');
+    		$user = $this->getDoctrine()->getRepository('FOPACChatBundle:Permit')->findOneBy(array('authHash' => $auth));
+
+    		if ($user) {
+    			$room = $user->getRoom();
+    			if ($room->getId() == $roomData['id'] && $user->isModerator() && $room->getPassphrase() == $roomData['pass']) { 
+    				if (empty($roomData['title'])) { $room->setTitle(md5(date("U"))); } else { $room->setTitle($roomData['title']); }
+    				$room->setDescription(htmlspecialchars($roomData['desc']));
+    				$room->setPassphrase($roomData['newPass']);
+
+    				$em = $this->getDoctrine()->getManager();
+    				$em->flush();
+
+    				$res = new JsonResponse();
+    				$res->setData(array('Status' => 0));
+    				return $res;
+    			}
+    		}
+    	}
+
+    	$res = new JsonResponse();
+    	$res->setData(array('Status' => 1));
+    	return $res;
+    }
+
     public function passCheckAction(Request $r)
     {    	
     	if ($r->isXmlHttpRequest() && $r->getMethod() == "POST") {
@@ -83,8 +111,143 @@ class RoomController extends Controller
     		}
     		
     		$res = new JsonResponse();
-    		$res->setData(array('Status' => 0));
+    		$res->setData(array('Status' => 1));
     		return $res;
     	}   	
+    }
+
+    public function infoAction(Request $r)
+    {    	
+    	if ($r->isXmlHttpRequest()) {
+    		$roomId = $r->get('roomId');  $passphrase = $r->get('passphrase');  $auth = $r->get('auth');
+    		$user = $this->getDoctrine()->getRepository('FOPACChatBundle:Permit')->findOneBy(array('authHash' => $auth));
+    		if ($user) {
+    			$room = $user->getRoom();
+    			if ($room->getId() == $roomId && $room->getPassphrase() == $passphrase) { 
+    				$permits = $room->getPermits();
+    				$perm = array();
+    				foreach ($permits as $i => $p) {
+    					$perm[$i] = array('nick' => substr($p->getAuthHash(), 0, 10),
+    									  'isModerator' => $p->isModerator());
+
+						$access = $p->getLastAccessAt();
+    					if ($access && (time() - $access->getTimestamp()) <= 60*5) { $perm[$i]['Online'] = true; }
+    					if ($user->isModerator()) {
+    						$perm[$i]['id'] = $p->getId();
+    						$perm[$i]['authHash'] = $p->getAuthHash();
+    						$perm[$i]['authURL'] = $this->get('router')->generate('chat_page', array(
+    																			  'permitId' => $p->getId(),
+    																			  'auth' => $p->getAuthHash()), true);
+    					}
+    				}
+
+    				$res = new JsonResponse();
+    				$res->setData(array('Status' => 0,
+    									'Room' => array('id' => $roomId,
+    													'title' => $room->getTitle(),
+    													'description' => $room->getDescription()),
+    									'Permits' => $perm));
+    				
+    				return $res;
+    			}
+    		}
+    		
+    		$res = new JsonResponse();
+    		$res->setData(array('Status' => 1));
+    		return $res;
+    	}   	
+    }
+
+    public function addPermitAction(Request $r)
+    {
+    	if ($r->isXmlHttpRequest() && $r->getMethod() == "POST") {
+    		$pass = $r->get('pass') ;  $auth = $r->get('auth');  $mod = $r->get('mod');
+    		$user = $this->getDoctrine()->getRepository('FOPACChatBundle:Permit')->findOneBy(array('authHash' => $auth));
+
+    		if ($user) {
+    			$room = $user->getRoom();
+    			if ($user->isModerator() && $room->getPassphrase() == $pass) { 
+    				$p = new Permit();
+    				$p->setRoom($room);
+    				$p->setAuthHash(hash('sha256', sprintf("%d|%s|%d|%d", $room->getId(), $room->getTitle(),
+    			time(), rand(0, 10000))));
+    				if ($mod == 'true') { $p->setModerator(true); } else { $p->setModerator(false); }    				
+
+    				$em = $this->getDoctrine()->getManager();
+    				$em->persist($p);
+    				$em->flush();
+
+    				$res = new JsonResponse();
+    				$res->setData(array('Status' => 0,
+    									'permitId' => $p->getId(),
+    									'authHash' => $p->getAuthHash(),
+    									'authURL' => $this->get('router')->generate('chat_page', array(
+    																			    'permitId' => $p->getId(),
+    																			    'auth' => $p->getAuthHash()), true),
+    									'mod' => $p->isModerator()));
+    				return $res;
+    			}
+    		}
+    	}
+
+    	$res = new JsonResponse();
+    	$res->setData(array('Status' => 1));
+    	return $res;
+    }
+
+    public function removePermitAction(Request $r)
+    {
+    	if ($r->isXmlHttpRequest() && $r->getMethod() == "POST") {
+    		$pass = $r->get('pass') ;  $auth = $r->get('auth');  $permitId = $r->get('id');
+    		$user = $this->getDoctrine()->getRepository('FOPACChatBundle:Permit')->findOneBy(array('authHash' => $auth));
+
+    		if ($user) {
+    			$room = $user->getRoom();
+    			if ($user->isModerator() && $room->getPassphrase() == $pass) { 
+    				$p = $this->getDoctrine()->getRepository('FOPACChatBundle:Permit')->find($permitId);
+    				if ($p)	{
+    					$em = $this->getDoctrine()->getManager();
+    					$em->remove($p);
+    					$em->flush();
+
+    					$res = new JsonResponse();
+    					$res->setData(array('Status' => 0));
+    					return $res;
+    				}    				
+    			}
+    		}
+    	}
+
+    	$res = new JsonResponse();
+    	$res->setData(array('Status' => 1));
+    	return $res;
+    }
+
+    public function changePermitAction(Request $r)
+    {
+    	if ($r->isXmlHttpRequest() && $r->getMethod() == "POST") {
+    		$pass = $r->get('pass') ;  $auth = $r->get('auth');  $permitId = $r->get('id');  $mod = $r->get('mod');
+    		$user = $this->getDoctrine()->getRepository('FOPACChatBundle:Permit')->findOneBy(array('authHash' => $auth));
+
+    		if ($user) {
+    			$room = $user->getRoom();
+    			if ($user->isModerator() && $room->getPassphrase() == $pass) { 
+    				$p = $this->getDoctrine()->getRepository('FOPACChatBundle:Permit')->find($permitId);
+    				if ($p)	{
+    					if ($mod == 'true') { $p->setModerator(true); } else { $p->setModerator(false); }
+    					$em = $this->getDoctrine()->getManager();    					
+    					$em->flush();
+
+    					$res = new JsonResponse();
+    					$res->setData(array('Status' => 0));
+    					return $res;
+    				}    				
+    			}
+    		}
+    	}
+
+    	$res = new JsonResponse();
+    	$res->setData(array('Status' => 1));
+    	return $res;
     }
 }
